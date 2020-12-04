19bb62b1e1299c64fb4413eebc33c65d2d1017bf
David Oster aka George Pasparakis <paspa@hotmail.com>
2020-12-04 17:25:27 +0200

    login servlet to run at startup

--- src/java/controllers/Login.java
+++ src/java/controllers/Login.java
@@ -17,30 +17,55 @@ import javax.servlet.http.HttpServletResponse;
  *
  * @author George.Pasparakis
  */
-@WebServlet(name = "login", urlPatterns = {"/login", "/Login"})
+@WebServlet(name = "login", urlPatterns = {"/", "/login", "/Login"})
 public class Login extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) 
-             throws ServletException, IOException { 
-         // here you listen for POST method
-         
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8"); // servlet
         try (PrintWriter out = response.getWriter()) {
             /* for the browser */
             out.println("<!DOCTYPE html>");
             out.println("<html>");
             out.println("<head>");
-            out.println("<title>Administration Panel - Precious EShop</title>");            
+            out.println("<title>Precious EShop</title>");
+            out.println("</head>");
+            out.println("<body>");
+            out.println("<h1>Login - Precious EShop" + "</h1>");
+            out.println(" <div>My Precious... EShop</div>\n" +
+                        "        <form action=\"/PreciousEShop/login\" method=\"POST\">\n" +
+                        "            Username :<input type=\"text\" name=\"username\" value=\"\" /><br/>\n" +
+                        "            Password :<input type=\"password\" name=\"password\" value=\"\" /><br/>\n" +
+                        "            <input type=\"submit\" value=\"Login\" />\n" +
+                        "        </form>");
+            out.println("</body>");
+            out.println("</html>");
+
+        }
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        // here you listen for POST method
+
+        response.setContentType("text/html;charset=UTF-8"); // servlet
+        try (PrintWriter out = response.getWriter()) {
+            /* for the browser */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Administration Panel - Precious EShop</title>");
             out.println("</head>");
             out.println("<body>");
             out.println("<h1>Administration Panel - Precious EShop" + "</h1>");
             String username = request.getParameter("username");
             String password = request.getParameter("password");
-            if(username.equalsIgnoreCase("admin") && password.equals("admin")) {
+            if (username.equalsIgnoreCase("admin") && password.equals("admin")) {
                 out.println("Welcome " + username + " to the app!" + " Your password is: " + password);
             }
             out.println("</body>");
             out.println("</html>");
         }
-     
-     }
+
+    }
 }
